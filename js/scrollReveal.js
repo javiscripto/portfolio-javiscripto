8e8e6920604af6bfba597fd02f42d7f827693244
javiscripto <javier.mecker94@gmail.com>
2024-09-23 10:23:49 -0300

    incorporación scrollReveal

---
+++ js/scrollReveal.js
@@ -0,0 +1,4 @@
+ScrollReveal({reset:true}).reveal('.container' , {delay:300});
+ScrollReveal().reveal('.proyectos-inner', {delay:900});
+ScrollReveal().reveal('.tecnologias-inner', {delay:700});
+ScrollReveal().reveal('.contacto-inner' , {delay:700});
